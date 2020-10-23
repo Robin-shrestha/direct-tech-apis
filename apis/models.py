30bc3fd90a682b65a3102113d95ea90fccd7d5f5
Robin-shrestha <rsresthas@gmail.com>
2020-10-23 14:16:55 +0545

    minor model change/ added title blank=True

--- apis/models.py
+++ apis/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Gallery(models.Model):
     artist = models.CharField(max_length=40, null=True, blank=True)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, blank=True)
     image = models.ImageField(upload_to='Gallery')
     date_added = models.DateField(auto_now_add=True)
 
